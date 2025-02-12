eff79bff6f94bf8bd1409692e6c169fa14d1b4af
hlcjunior <62648013+hlcjunior@users.noreply.github.com>
2025-02-12 16:45:20 -0300

    Melhorias imports

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
-use Illuminate\Http\Request;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 
 class RegisterController extends Controller
 {
-    public function index()
+    public function index(): View|Application|Factory
     {
         return view('auth.register');
     }
 
-    public function register( RegisterRequest $request)
+    public function register( RegisterRequest $request): RedirectResponse
     {
         if($request->tryToRegister()){
             return to_route('dashboard');
