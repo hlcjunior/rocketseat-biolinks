12e5b2346b13951236d4b9e6986da7090d5bee8b
hlcjunior <62648013+hlcjunior@users.noreply.github.com>
2025-02-14 21:12:08 -0300

    Adicionando Policies

---
+++ app/Policies/LinkPolicy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Link;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class LinkPolicy
+{
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function denyAll(User $user, Link $link): Response
+    {
+        return $link->user->is($user)
+            ? Response::allow()
+            : Response::deny('Você não tem acesso a este link.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,15 @@ Route::middleware('auth')->group(function (){
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
 
-    Route::get('/links/create', [LinkController::class,'create'])->name('links.create');
-    Route::post('/links/create', [LinkController::class,'store']);
-    Route::get('/links/{link}/edit', [LinkController::class,'edit'])->name('links.edit');
-    Route::put('/links/{link}/edit', [LinkController::class,'update']);
-    Route::delete('/links/{link}', [LinkController::class,'destroy'])->name('links.destroy');
-
-    Route::patch('/links/{link}/up', [LinkController::class, 'up'])->name('links.up');
-    Route::patch('/links/{link}/down', [LinkController::class, 'down'])->name('links.down');
+    Route::middleware('can:denyAll,link')->group(function (){
+        Route::get('/links/create', [LinkController::class,'create'])->name('links.create');
+        Route::post('/links/create', [LinkController::class,'store']);
+        Route::get('/links/{link}/edit', [LinkController::class,'edit'])->name('links.edit');
+        Route::put('/links/{link}/edit', [LinkController::class,'update']);
+        Route::delete('/links/{link}', [LinkController::class,'destroy'])->name('links.destroy');
+        Route::patch('/links/{link}/up', [LinkController::class, 'up'])->name('links.up');
+        Route::patch('/links/{link}/down', [LinkController::class, 'down'])->name('links.down');
+    });
 
     Route::get('/logout', LogoutController::class)->name('logout');
 });
